06338249e20ba0a8d56cd8d8a99cb71ac9fcd041
robiulalam76 <robiulalam762400@gmail.com>
2022-10-24 23:26:51 +0600

    header add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { Bars3Icon } from 'react-icons/fa';
-import { AuthContext } from '../../ContextAPI/AuthProvider';
+import { AuthContext } from '../../ContextAPI/AuthContext';
+import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
+
 
 const Header = () => {
     const { user, logOut } = useContext(AuthContext)
@@ -17,46 +18,52 @@ const Header = () => {
 
     return (
         <section className=''>
-            <nav className='py-2 px-4 md:px-8 relative'>
+            <nav className='bg-blue-100 py-2 px-4 md:px-8 relative'>
                 <div onClick={() => setOpen(!open)} className='text-blue-600 w-10 absolute right-4 md:right-14 md:hidden'>
                     {
-                        open ? <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#000" stroke-width="2" d="M3,3 L21,21 M3,21 L21,3"></path></svg>
-                            :
-                            <Bars3Icon></Bars3Icon>
+                        open ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>
                     }
                 </div>
 
                 <div className='flex md:items-center md:justify-end flex-col md:flex-row items-start font-bold'>
-                    <NavLink to='/home' className='flex-grow flex items-center font-bold text-2xl text-blue-600'> <img src="logo.png" className='w-10' alt="" />TRAVELIFY</NavLink>
+                    <NavLink to='/home' className='flex-grow flex items-center font-bold text-2xl text-blue-600'> <img src="logo.png" className='w-44' alt="" /></NavLink>
                     <div className={`flex md:items-center md:justify-end flex-col md:flex-row items-start text-black font-bold ml-10 mt-3 md:m-0 md:block duration-500 ease-in ${open ? 'block' : 'hidden'}`}>
                         <NavLink to='/home'
                             className={({ isActive }) => isActive ?
                                 'underline underline-offset-4  decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 hover:text-blue-700'}>Home</NavLink>
-                        <NavLink to='/news'
+                        <NavLink to='/courses'
                             className={({ isActive }) => isActive ?
-                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>News</NavLink>
-                        <NavLink to='/destination'
-                            className={({ isActive }) => isActive ?
-                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Destination</NavLink>
+                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Courses</NavLink>
                         <NavLink to='/blog'
                             className={({ isActive }) => isActive ?
                                 'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Blog</NavLink>
-                        <NavLink to='/contact'
+                        <NavLink to='/about'
                             className={({ isActive }) => isActive ?
-                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Contact</NavLink>
-                        <NavLink to='/login'
-                            className={({ isActive }) => isActive ?
-                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Login</NavLink>
-
+                                'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>About</NavLink>
 
                         {
-                            user?.photoURL ? <Link onClick={() => setProfileView(!profileView)} className='order-first'>
-                                <img className='rounded-full w-10 inline border-2 border-blue-600' src={user?.photoURL} alt="" />
-                            </Link> :
-                                <Link onClick={() => setProfileView(!profileView)} className='order-first'>
-                                    <img className='rounded-full w-10 inline border-2 border-blue-600' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
-                                </Link>
+                            user?.uid ?
+                                <>
+                                    {
+                                        user?.photoURL ?
+                                            <Link onClick={() => setProfileView(!profileView)} className='order-first'>
+                                                <img className='rounded-full w-10 inline border-2 border-blue-600' src={user?.photoURL} alt="" />
+                                            </Link> :
+                                            <Link onClick={() => setProfileView(!profileView)} className='order-first'>
+                                                <img className='rounded-full w-10 inline border-2 border-blue-600' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" />
+                                            </Link>
+
+                                    }
+                                </>
+                                :
+                                <NavLink to='/login'
+                                    className={({ isActive }) => isActive ?
+                                        'mt-3 underline underline-offset-4 decoration-2 text-rose-600 md:mr-10' : 'md:mr-10 mt-3 hover:text-blue-700'}>Login</NavLink>
+
                         }
+
+
+
 
                     </div>
                 </div>
--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,14 +5,26 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyACQBIUqnvEiUESWh7jEHig3jlAxSm4-uE",
+    authDomain: "the-learning-hub-b3af3.firebaseapp.com",
+    projectId: "the-learning-hub-b3af3",
+    storageBucket: "the-learning-hub-b3af3.appspot.com",
+    messagingSenderId: "539388438749",
+    appId: "1:539388438749:web:6e141a39dce8a7e5fc9334"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId,
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 export default app;
