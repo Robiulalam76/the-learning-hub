f1d3288269b14c5fd5d90e59a242beaeab161ad6
robiulalam76 <robiulalam762400@gmail.com>
2022-10-25 15:52:50 +0600

    side bar add

--- src/Components/CourseCard/CourseCard.js
+++ src/Components/CourseCard/CourseCard.js
@@ -2,20 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CourseCard = ({ course }) => {
-    const { id, name, thumbnail, time, lasson, ratings, coursePrice, instructor } = course
+    const { id, name, thumbnail, time, lesson, ratings, coursePrice, instructor } = course
+    console.log(thumbnail);
     return (
         <div>
-            <div className="relative max-w-sm shadow-lg shadow-gray-400 overflow-hidden shadow-lg rounded-lg mx-auto mt-4">
-                <img className="w-full rounded-t-lg h-44" src={course.thumbnail} alt="Sunset in the mountains" />
-                <div className="px-6 py-4">
-                    <h1 className="font-extrabold text-blue-900 text-xl mb-2">{name}</h1>
-                    <p className="text-gray-800 text-base">
-                        {instructor}
-                    </p>
+            <div className="relative max-w-sm shadow-lg shadow-gray-400  shadow-lg rounded-lg mx-auto mt-4
+            bg-gradient-to-r from-indigo-100 via-purple-50 to-blue-100">
+                <img className="w-full rounded-t-lg h-44" src={thumbnail} alt="Sunset in the mountains" />
+
+                <div className='px-2'>
+                    <div className="py-4">
+                        <h1 className="font-extrabold text-blue-900 text-xl mb-2">{name}</h1>
+                        <p className="text-gray-800 text-base">
+                            {instructor}
+                        </p>
+                    </div>
+                    <div className='flex justify-between items-center my-4'>
+                        <h1 className='font-extrabold text-rose-600 text-3xl'>${coursePrice}</h1>
+                        <div className='flex justify-between items-center'>
+
+                            <span>
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M18 11c.34 0 .67.03 1 .08V4c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h7.26A6.995 6.995 0 0118 11zM7 11V4h5v7L9.5 9.5 7 11z"></path><path d="M18 13c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm-1.25 7.5v-5l4 2.5-4 2.5z"></path></svg>
+                            </span>
+                            <h1 className='font-bold text-blue-900'>Lesson: {lesson}</h1>
+                        </div>
+
+                        <div className='text-yellow-500 flex justify-between items-center'>
+                            <span className='text-blue-900 mr-1'>{ratings}</span>
+                            <span>
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>
+                            </span>
+                            <span>
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>
+                            </span>
+                            <span>
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>
+                            </span>
+                            <span>
+                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"></path></svg>
+                            </span>
+                        </div>
+                    </div>
                 </div>
                 <div className="pt-8">
                     <Link className='absolute bottom-0 w-full'>
-                        <button className='py-2 px-3 bg-blue-600 hover:bg-blue-800 font-bold rounded-b-lg text-white w-full'>Course Details</button>
+                        <button className='py-2 px-3 font-bold rounded-b-lg text-white w-full bg-gradient-to-r from-blue-600 via-purple-500 to-blue-600 hover:bg-gradient-to-r hover:from-blue-900 hover:via-purple-800 hover:to-blue-900 
+                        '>Course Details</button>
                     </Link>
                 </div>
             </div>
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import CourseCard from '../CourseCard/CourseCard';
 
 const Courses = () => {
     const courses = useLoaderData()
-    const [course, setCourse] = useState()
     // console.log(courses)
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16'>
-            {
-                courses.map(course => <CourseCard
-                    key={course.id}
-                    course={course}
-                ></CourseCard>)
-            }
+        <div className='grid md:grid-cols-6 gap-4 relative'>
+            <div className='md:col-span-1 bg-violet-500 pt-8 w-full'>
+                <h1 className='bg-blue-50 py-2 text-center font-bold text-blue-900 md:1xl lg:text-2xl'>Total Courses {courses.length}</h1>
+                <div className='grid grid-cols-3 md:grid-cols-none px-2 md:pl-8 text-center md:text-left'>
+                    {
+                        courses.map(course =>
+                            <Link className='md:block font-bold text-white hover:text-blue-600 pt-3' to={``}>{course.name}
+                            </Link>)
+                    }
+                </div>
+            </div>
+            <div className='md:col-span-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12 md:px-6 w-[90%] md:w-full mx-auto'>
+                {
+                    courses.map(course => <CourseCard
+                        key={course.id}
+                        course={course}
+                    ></CourseCard>)
+                }
+            </div>
         </div>
     );
 };
