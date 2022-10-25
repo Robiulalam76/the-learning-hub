688894f72c74f82cede8d6e8bc212d0cadedd19f
robiulalam76 <robiulalam762400@gmail.com>
2022-10-25 23:55:34 +0600

    about page added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,20 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>this is about page</h1>
+            <div class="py-16 bg-white">
+                <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                    <div class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                        <div class="md:5/12 lg:w-5/12">
+                            <img src="https://lh3.googleusercontent.com/PEU6mQHnbBX4YCh8pk2mKOocas_XN5N1vYpGQv7U7p-SCWKDTYOdXYtRvYysAGqh0Y8ssrsqo1cB3Np9XPPRyhjTpjdYJ6B7CXiZrxCwulAmAeoKAeN8tEQ_qTgdoTtRr-zALs3rb2K3poLlqrLWT3grsY8yQIYIOZHKz5rEoTYJ28WjFtb3EGsgr7qirzP76Mctph98X5loobZW0ILg2k4-ZuBf_xw1ZMthadpBOjHOV1g1VHsP1S7BuG3uw7rCePpssHEaC2-hU9rfp1chX8hhFRb-YMIlgARGYY13MWzTu8dAArypWVe-goKmItPduvbqhaW83PDrlhS0ZO2d2uKHn0fRkBxuKAQUXa4J-rqrko4SODTKV2b7bVkAdNEeRzcs7gC3FKt-LDYRMPz7XoFq1kK-7UVnZIcQUtPXYT6DVOyJrl4VkLeDf_DfD5jHSAjHj2PFktErjgTeKUNQeHM7K81SthpNShQCFVeON9JKLUKETikS-q7kLiJ83PM5pbwzZa_Dys_TwsQy93_2JlhRa92Ojrf6cEb2-SI465qEOaGFd3tQyjSW4EFusei3bVzV354OPzUecpYqnJq-ASh7vPuT0qtgwqulGZFlu1sjqY8c6NRKpVigbQ5C0iAwk0VC1ACCULSo7Luqk9FIKKqPtfpeEp9fiWRFFFJOOqxXOHuFmp9Tw1_c7CIOng0w3kPxjRucPiLiTG60Ud7CLw6tiQhu7SIHSE5eb_H018jf4wpMLUEdf8pryZuts-vJXrFHl-Vya4pEWdqqkfwWG8HHvS5b3B5becWWDxbtUYAZXmwufJ4bFibyxJGz4c4EJ-nnrs8bVQDzpW3C-EgudmPL5clMZDSZBRrIJsp7PnnpNLsUn4sOnMVDE-TzSgoWmBhLsnVq6CQevLpHwHWc7O4V6lCWzJLb53W7D3StHxItqy04yOu2qvxK__bCshX--_Yx0s8rpA=s328-no?authuser=0" alt="about-img" />
+                        </div>
+                        <div class="md:7/12 lg:w-6/12">
+                            <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Nuxt development is carried out by passionate developers</h2>
+                            <p class="mt-6 text-gray-600">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!</p>
+                            <p class="mt-4 text-gray-600"> Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
