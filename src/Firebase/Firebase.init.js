e56a11e5078ddef24644dc149eed08a0d93639e4
robiulalam76 <robiulalam762400@gmail.com>
2022-10-26 11:07:00 +0600

    variable enviroment add

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyACQBIUqnvEiUESWh7jEHig3jlAxSm4-uE",
-    authDomain: "the-learning-hub-b3af3.firebaseapp.com",
-    projectId: "the-learning-hub-b3af3",
-    storageBucket: "the-learning-hub-b3af3.appspot.com",
-    messagingSenderId: "539388438749",
-    appId: "1:539388438749:web:6e141a39dce8a7e5fc9334"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
